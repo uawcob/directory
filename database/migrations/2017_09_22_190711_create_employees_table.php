16a0df4fcfa1cc6b24d6918f2ffb98b59ae4aa3d
Jeff Puckett <jeff@jeffpuckett.com>
2017-09-27 18:54:06 +0000

    test on mysql

--- database/migrations/2017_09_22_190711_create_employees_table.php
+++ database/migrations/2017_09_22_190711_create_employees_table.php
@@ -24,7 +24,7 @@ class CreateEmployeesTable extends Migration
             $table->string('middlename', 15)->nullable();
             $table->string('lastname', 30)->nullable();
             $table->string('BU_Code', 4)->nullable();
-            $table->string('BU_Short_Name', 24)->nullable();
+            $table->string('BU_Short_Name', 26)->nullable();
             $table->string('dir_addr', 40)->nullable();
             $table->string('dir_city', 25)->nullable();
             $table->string('dirstate', 8)->nullable();
@@ -40,7 +40,7 @@ class CreateEmployeesTable extends Migration
             $table->string('emp_priv', 1)->nullable();
             $table->date('lasthire')->nullable();
             $table->date('dbuasrvc')->nullable();
-            $table->string('preftitl', 30)->nullable();
+            $table->string('preftitl', 39)->nullable();
             $table->string('username', 8)->nullable();
             $table->string('studtitl', 1)->nullable();
             $table->string('emp_pos', 3)->nullable();
